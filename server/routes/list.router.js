4345788775ee634712348b703fa88f45afcd76bd
Isiah Smith <isiahthomassmith@gmail.com>
2021-10-16 15:24:18 -0500

    GET made on server side

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,6 +2,17 @@ const express = require('express');
 const listRouter = express.Router();
 const pool = require(`../modules/pool.js`);
 
+listRouter.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "to_do_list" ORDER BY "id" DESC;`;
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting tasks', error);
+        res.sendStatus(500);
+    });
+});
+
+
 
 
 
